d89431769051f85c4e88b8c3dc3f57687d1c6f1d
koladev <onaelmangabo@gmail.com>
2021-06-08 02:01:35 +0100

    - feat: add tailwind configurations

--- tasks-app/src/index.tsx
+++ tasks-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {ApolloProvider} from '@apollo/client';
 import apolloClient from './apollo/config';
 
